8e9eb1c19b4d63c00cf41e056baf4057bb93c228
woniu336 <woniu336@users.noreply.github.com>
2024-06-12 19:17:03 +0000

    update woniu336/stack@39b3f9b215aa66facd80d436efdb8dcd5a12a6d7

--- js/tiaozhuan.js
+++ js/tiaozhuan.js
@@ -17,8 +17,29 @@ target = target.parentNode;
 if (target && target.nodeName === 'A' &&
 !checkParent(target, excludedClasses) &&
 !target.href.includes('blog.taoshuge.eu.org') &&
+!target.href.includes('railway.app') &&
+!target.href.includes('cloudflare.com') &&
+!target.href.includes('gohugo.io') &&
+!target.href.includes('vercel.com') &&
+!target.href.includes('umami.is') &&
+!target.href.includes('aliyundrive.com') &&
+!target.href.includes('youtube.com') &&
+!target.href.includes('chatgpt.com') &&
+!target.href.includes('x.com') &&
+!target.href.includes('moonshot.cn') &&
+!target.href.includes('baidu.com') &&
+!target.href.includes('csdn.net') &&
+!target.href.includes('gitee.com') &&
+!target.href.includes('51.la') &&
+!target.href.includes('supabase.com') &&
 !target.href.includes('123pan.com') &&
+!target.href.includes('huggingface.co') &&
+!target.href.includes('ollama.com') &&
+!target.href.includes('99bilibili.eu.org') &&
+!target.href.includes('indiehackertools.net') &&
 !target.href.includes('douban.com') &&
+!target.href.includes('discord.com') &&
+!target.href.includes('coze.com') &&
 !target.href.includes('bilibili.com') &&
 !target.href.includes('github.com') &&
 target.hostname !== window.location.hostname) {
